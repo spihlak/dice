6bea981bb91e6a7225a7af3e507940a9cf5eb3c5
Siim Pihlak <siimpihlak@Siims-Mac-mini.local>
2025-05-23 17:54:44 +0300

    Dots Development

--- script.js
+++ script.js
@@ -1,15 +1,34 @@
 const dice = document.getElementById("dice");
 const btn = document.getElementById("diceRoll");
+const face = document.getElementById("diceFace");
 
-dice.textContent = 6;
+initDice(6);
 
 btn.addEventListener("click", () => {
     dice.classList.add("fade");
     btn.classList.add("click");
+    face.removeAttribute("class");
+    initDice(0);
     setTimeout(() => {
         num = Math.floor(Math.random() * 6) + 1;
-        dice.textContent = num;
+        initDice(num);
         dice.classList.remove("fade");
         btn.classList.remove("click");
     }, "300");
 });
+
+function initDice(num) {
+    if(num > 0) {
+    face.classList.add("df-"+ num +"-dots");
+        for (let i = 0; i < num; i++) {
+            const dot = document.createElement("div");
+            dot.classList.add("dot");
+            face.appendChild(dot);
+        }
+    } else {
+        let dots = document.querySelectorAll(".dot");
+        dots.forEach(dot => {
+            dot.remove();
+        });
+    }
+}
